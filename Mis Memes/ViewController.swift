bc5ec4edf2a5f39d1e42add23824096abeca8d87
Christyan Duarte <accord29@icloud.com>
2018-11-09 11:36:34 -0600

    Initial Commit

---
+++ Mis Memes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mis Memes
+//
+//  Created by Christyan Duarte on 11/9/18.
+//  Copyright © 2018 Making your app. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Mis MemesTests/Mis_MemesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Mis_MemesTests.swift
+//  Mis MemesTests
+//
+//  Created by Christyan Duarte on 11/9/18.
+//  Copyright © 2018 Making your app. All rights reserved.
+//
+
+import XCTest
+@testable import Mis_Memes
+
+class Mis_MemesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
