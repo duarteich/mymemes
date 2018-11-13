c77507a252bc3aedd7bd30c3b5b6c65ec2d5dc67
= <=>
2018-11-12 23:09:08 -0600

    Initial commit

---
+++ Mis Memes/Meme.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  Mis Memes
+//
+//  Created by Christyan Duarte on 11/12/18.
+//  Copyright © 2018 Making your app. All rights reserved.
+//
+
+import Foundation
+
+struct Meme: Codable {
+    var name: String
+    var imageName: String
+
+}
+
--- Mis Memes/ViewController.swift
+++ Mis Memes/ViewController.swift
@@ -8,13 +8,100 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    fileprivate let itemsPerRow: CGFloat = 4
+    fileprivate let sectionInsets = UIEdgeInsets(top: 10.0, left: 10.0, bottom: 10.0, right: 10.0)
+    let memesPath = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent("memes")
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    @IBAction func addMeme(_ sender: UIBarButtonItem) {
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    let imagePicker = UIImagePickerController()
+    var memes = [Meme]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        imagePicker.delegate = self
+        loadMemes()
+    }
+    
+    private func loadMemes() {
+        if let data = UserDefaults.standard.value(forKey:"memes") as? Data {
+            guard let memes = try? PropertyListDecoder().decode(Array<Meme>.self, from: data) else { return }
+            self.memes = memes
+            collectionView.reloadData()
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[.editedImage] as? UIImage {
+            saveImageDocumentDirectory(image: pickedImage, imageName: "meme_1")
+            let meme = Meme(name: "Meme 1", imageName: "meme_1")
+            memes.append(meme)
+            UserDefaults.standard.set(try? PropertyListEncoder().encode(memes), forKey:"memes")
+            loadMemes()
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func saveImageDocumentDirectory(image: UIImage, imageName: String) {
+        let fileManager = FileManager.default
+        if !fileManager.fileExists(atPath: memesPath) {
+            try! fileManager.createDirectory(atPath: memesPath, withIntermediateDirectories: true, attributes: nil)
+        }
+        let url = NSURL(string: memesPath)
+        let imagePath = url?.appendingPathComponent(imageName)
+        let urlString: String = imagePath!.absoluteString
+        let imageData = image.jpegData(compressionQuality: 0.8)
+        fileManager.createFile(atPath: urlString as String, contents: imageData, attributes: nil)
+    }
+    
+    func getDirectoryPath() -> NSURL {
+        let path = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent("memes")
+        let url = NSURL(string: path)
+        return url!
+    }
+    
+    func getImage(imageName: String) -> UIImage? {
+        let fileManager = FileManager.default
+        let url = NSURL(string: memesPath)
+        guard let imagePath = url?.appendingPathComponent(imageName) else { return nil }
+        if fileManager.fileExists(atPath: imagePath.absoluteString) {
+            return UIImage(contentsOfFile: imagePath.absoluteString)
+        }
+        return nil
     }
 
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "memeCell", for: indexPath)
+        if let image = getImage(imageName: memes[indexPath.row].imageName) {
+            cell.backgroundView = UIImageView(image: image)
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
 }
 
